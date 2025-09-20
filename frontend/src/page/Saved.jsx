335a414d14f76b9e4fc7ee45dd58200698280a73
Lin-shao-an <linbrian9498@gmail.com>
2025-09-20 16:11:06 +0800

    feat: implement saved jobs page layout and functionality with conditional rendering

--- frontend/src/page/Saved.jsx
+++ frontend/src/page/Saved.jsx
@@ -1,18 +1,38 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './Profile.css';
-import Navigation from '../components/navigation';
+import React, { useState, useEffect, useRef } from "react";
+import "./Saved.css";
+import Navigation from "../components/navigation";
 
 function SavedPage() {
+  const [saved_jobs, set_saved_jobs] = useState([]);
 
-  const saved_jobs = ['googoogaga', 'HAHA', 'AMD', 'google'];
+  // useEffect(() => {
+  //   get_saved_jobs()
+  //     .then((response) => {
+  //       console.log("Get saved jobs successful:", response);
+  //       setJobs(response);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Get saved jobs failed:", error);
+  //     });
+  // }, []);
 
   return (
     <div className="saved-content-page">
       <h3>Saved works</h3>
-      <div className="profile-info">
-        
+      <div className="saved-info">
+        <div className="saved-list">
+          {saved_jobs.length > 0 ? (
+            saved_jobs.map((job, index) => (
+              <div className="saved-item" key={index}>
+                <JobElement job={job} />
+              </div>
+            ))
+          ) : (
+            <p className="no-result-text">Nothing Saved</p>
+          )}
+        </div>
       </div>
-      <Navigation/>
+      <Navigation />
     </div>
   );
 }

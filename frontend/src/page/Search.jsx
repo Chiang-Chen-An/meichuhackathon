81f5b35dd7760f3f3451b683ef11a27d074856c1
Lin-shao-an <linbrian9498@gmail.com>
2025-09-20 10:46:51 +0800

    feat: update layout and styling for responsive design in app and search components

--- frontend/src/page/Search.jsx
+++ frontend/src/page/Search.jsx
@@ -5,6 +5,10 @@ import "./Search.css";
 function SearchPage() {
   return (
     <>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      ></link>
       <div className="content-page">
         <div className="search-bar">
           <input
@@ -12,7 +16,9 @@ function SearchPage() {
             placeholder="Search for jobs, location, company"
             className="search-input"
           />
-          <button className="search-button">search</button>
+          <button className="search-button">
+            <i class="fa fa-search"></i>
+          </button>
         </div>
         <p className="no-result-text">nothing be found</p>
       </div>

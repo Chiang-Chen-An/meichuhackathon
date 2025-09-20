1f3ff696b783598299bc87a422703c6d11eba81f
Chiang-Chen-An <aron100096@gmail.com>
2025-09-20 10:19:40 +0800

    fix: correct axios post request syntax and ensure consistent base_url declaration

--- frontend/src/route/user.js
+++ frontend/src/route/user.js
@@ -2,13 +2,27 @@ import axios from 'axios';
 
 export async function login(data) {
     try {
-        base_url = 'backend:8000'
-        const response = await axios({
+        base_url = 'backend:8000';
+        const response = await axios.post({
             url: base_url,
             data
         });
         return response.data;
     } catch (error) {
         throw error;
+    }
+}
+
+
+export async function register(data) {
+    try {
+        base_url = 'backend:8000';
+        const response = await axios.post({
+            url: base_url,
+            data
+        });
+        return response.data;
+    } catch (error) {
+        throw response.data;
     }
 }
